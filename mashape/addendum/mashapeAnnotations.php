1ca40b933165129d88d697c4650ab6d2883966cf
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-28 03:33:07 -0700

    Added methods annotations

--- mashape/addendum/mashapeAnnotations.php
+++ mashape/addendum/mashapeAnnotations.php
@@ -1,5 +1,17 @@
 <?php
 require "annotations.php";
 
-class Persistent extends Annotation {}
+//
+// Methods annotations
+//
+
+// HTTP methods
+class GET extends Annotation { }
+class POST extends Annotation { }
+class PUT extends Annotation { }
+class DELETE extends Annotation { }
+
+class Route extends Annotation { }
+class Result extends Annotation { }
+    
 ?>
